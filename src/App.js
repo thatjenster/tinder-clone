67c22a4094eed4a79c0b9a60a7d2c6d959176c36
Jenny Dinh <jennythi.d@gmail.com>
2020-11-29 21:44:31 -0500

    add function btn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './styles/styles.css'
 import Header from './components/Header'
 import Cards from './components/Cards'
+import SwipeButton from './components/SwipeButton'
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Cards />
+      <SwipeButton />
     </div>
   );
 }
